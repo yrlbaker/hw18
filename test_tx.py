b4c55f0f5c63d7e9d3fbea9376e84ccee57654d7
yrlbaker <curativefinance@gmail.com>
2021-07-22 18:14:01 -0500

    Added/edited test transactions

--- test_tx.py
+++ test_tx.py
@@ -1,2 +1,11 @@
 # This code will send a single test transaction.
 
+from web3 import Web3
+
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
+
+print(w3.eth.blockNumber)
+
+print(w3.eth.getBalance
+("0x06AaC4992d3559b612eDEc8b46db40B59D3483E3"
+))
